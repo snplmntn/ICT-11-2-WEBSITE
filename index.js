4cf1c2b8cc2cc16e1679df5bfca51e0e2597701c
snplmntn <124994202+snplmntn@users.noreply.github.com>
2023-03-22 13:36:33 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const to_signup_button = document.querySelector(".to-signup-button");
+const to_login_button = document.querySelector(".to-login-button");
+const signupContainer = document.querySelector(".container-signup");
+const loginContainer = document.querySelector(".container-login");
+
+if (localStorage.getItem("signUp") === "false") {
+  signupContainer.classList.remove("hidden");
+}
+
+if (localStorage.getItem("logIn") === "true") {
+  loginContainer.classList.add("hidden");
+}
+
+const login_signup = function () {
+  signupContainer.classList.toggle("hidden");
+  loginContainer.classList.toggle("hidden");
+  localStorage.setItem("signUp", signupContainer.classList.contains("hidden"));
+  localStorage.setItem("logIn", loginContainer.classList.contains("hidden"));
+};
+
+to_signup_button.addEventListener("click", login_signup);
+to_login_button.addEventListener("click", login_signup);
