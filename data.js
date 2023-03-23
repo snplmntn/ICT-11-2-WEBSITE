e0fb7baf14f9ae19f4751205c03c71a97b74ec58
snplmntn <124994202+snplmntn@users.noreply.github.com>
2023-03-23 12:50:13 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -25,7 +25,7 @@ function checkAuthState() {
   auth.onAuthStateChanged(function (user) {
     if (load) {
       if (user) {
-        window.location.href = "/dashboard.html";
+        window.location.href = "/ICT-11-2-WEBSITE/dashboard.html";
       }
       load = false;
     }
@@ -86,7 +86,7 @@ function signup() {
 
       // Declare user variable
       var user = auth.currentUser;
-
+      console.log(user);
       // Get a reference to the "users" node with the user's UID as the child node
       var databaseRef = ref(database, "users/" + user.uid);
 
--- script.js
+++ script.js
@@ -14,6 +14,8 @@ import {
   set,
   update,
   get,
+  push,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js";
 
 const firebaseConfig = {
@@ -27,18 +29,42 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const database = getDatabase();
 
-// Logging the user name
-let userName = localStorage.getItem("userName");
-const name = document.querySelector("#user-name");
-name.textContent = userName;
+let clientUser;
+function checkAuthState() {
+  auth.onAuthStateChanged(function (user) {
+    if (user) {
+      overlay.classList.add("hidden");
+      const databaseRef = ref(database, "users/" + user.uid);
+      clientUser = auth.currentUser;
+      console.log(clientUser);
 
-//Log out
+      get(databaseRef).then((client) => {
+        // console.log("Name:", user_data.val().name);
+        const name = document.querySelector("#user-name");
+        name.textContent = client.val().name;
+        clientUser = client;
+      });
+    } else {
+      window.location.href = "/ICT-11-2-WEBSITE/index.html";
+    }
+  });
+}
+
+const loader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  loader.classList.add("hidden");
+  overlay.classList.add("hidden");
+  checkAuthState();
+});
+
 const logOut = document.querySelector("#logout-button");
 const logOutLink = document.querySelector("#logout-link");
 const exitBtn = document.querySelectorAll(".btn-close");
-const overlay = document.querySelector(".overlay");
 const announce = document.querySelector("#add-announcement");
+const overlay = document.querySelector(".overlay");
 
 logOut.addEventListener("click", function () {
   // Call the signOut() method to log out the user
@@ -57,6 +83,7 @@ function toggleModal() {
   document.querySelector(".overlay").classList.add("hidden");
   document.querySelector(".logout-modal").classList.add("hidden");
   document.querySelector(".announce-container").classList.add("hidden");
+  document.querySelector(".upload-post-container").classList.add("hidden");
 }
 
 logOutLink.addEventListener("click", function () {
@@ -87,40 +114,138 @@ const submitAnnounce = document.querySelector("#submit-btn");
 const announceContainer = document.querySelector("#new-announcements");
 
 submitAnnounce.addEventListener("click", function () {
-  //CREATE NEW ELEMENTS
-  const divCreate = document.createElement("DIV");
-  const titleCreate = document.createElement("H2");
-  const contentCreate = document.createElement("P");
-
   const titleValue = document.querySelector("#title");
   const contentValue = document.querySelector("#content");
-  const titleArrays = [];
-  const contentArrays = [];
-  titleArrays.push(titleValue.value);
-  contentArrays.push(contentValue.value);
 
-  titleCreate.textContent = "test title";
-  contentCreate.textContent = "test content";
-  announceContainer.appendChild(divCreate);
-  divCreate.appendChild(titleCreate);
-  divCreate.appendChild(contentCreate);
+  const announcement = {
+    author: clientUser.val().name,
+    title: titleValue.value,
+    content: contentValue.value,
+  };
+
+  const announcementsRef = ref(database, "announcements/" + Date.now());
+  set(announcementsRef, announcement);
+
+  titleValue.value = "";
+  contentValue.value = "";
   toggleModal();
 });
 
-function checkAuthState() {
-  auth.onAuthStateChanged(function (user) {
-    if (user) {
-      overlay.classList.add("hidden");
-    } else {
-      window.location.href = "/index.html";
+const announcementsRef = ref(database, "announcements");
+let announcementTitles = [];
+
+onValue(announcementsRef, (snapshot) => {
+  snapshot.forEach((childSnapshot) => {
+    const announcement = childSnapshot.val();
+    const contentValue = announcement.content;
+    const titleValue = announcement.title;
+
+    let titleExists = false;
+    for (let i = 0; i < announcementTitles.length; i++) {
+      if (announcementTitles[i] === titleValue) {
+        titleExists = true;
+        break;
+      }
+    }
+
+    if (!titleExists) {
+      const divCreate = document.createElement("div");
+      divCreate.classList.add("announcement");
+      const titleCreate = document.createElement("h2");
+      const contentCreate = document.createElement("p");
+
+      announcementTitles.push(titleValue);
+
+      titleCreate.innerHTML = titleValue;
+      contentCreate.innerHTML = contentValue;
+
+      if (document.querySelector(".announcement")) {
+        const firstChild = announceContainer.firstChild;
+        announceContainer.insertBefore(divCreate, firstChild);
+      } else {
+        announceContainer.appendChild(divCreate);
+      }
+
+      divCreate.appendChild(titleCreate);
+      divCreate.appendChild(contentCreate);
     }
   });
-}
+});
 
-const loader = document.querySelector(".preloader");
+// POST FUNCTIONS!!
+const createPostBtn = document.querySelector(".create-post");
+const textArea = document.querySelector("#textarea");
 
-window.addEventListener("load", function () {
-  loader.classList.add("hidden");
-  overlay.classList.add("hidden");
-  checkAuthState();
+createPostBtn.addEventListener("click", function () {
+  document.querySelector(".overlay").classList.remove("hidden");
+  document.querySelector(".upload-post-container").classList.remove("hidden");
+});
+
+//designs for textarea
+textArea.addEventListener("keyup", function (e) {
+  textArea.style.height = "auto";
+  let scrollHeight = e.target.scrollHeight;
+  textArea.style.height = `${scrollHeight}px`;
+});
+
+//appending posts
+const post = document.querySelector("#post-btn");
+const allPosts = document.querySelector("#dos");
+const postArrays = [];
+let j = 0;
+post.addEventListener("click", function () {
+  //CREATE NEW ELEMENTS
+  const postDivCreate = document.createElement("div");
+  postDivCreate.classList.add("post");
+
+  const postContent = {
+    author: clientUser.val().name,
+    content: textArea.value,
+    postedBy: `Posted By: ${clientUser.val().name}`,
+  };
+
+  const postRefs = ref(database, "posts/" + Date.now());
+  set(postRefs, postContent);
+
+  toggleModal();
+  textArea.value = "";
+});
+
+const postRefs = ref(database, "posts/");
+let postContents = [];
+
+onValue(postRefs, (snapshot) => {
+  snapshot.forEach((childSnapshot) => {
+    const post = childSnapshot.val();
+    const contentValue = post.content;
+    const postedBy = post.postedBy;
+
+    let postExists = false;
+    for (let i = 0; i < postContents.length; i++) {
+      if (postContents[i] === contentValue) {
+        postExists = true;
+        break;
+      }
+    }
+
+    if (!postExists) {
+      const postDivCreate = document.createElement("div");
+      postDivCreate.classList.add("post");
+      const postContentCreate = document.createElement("p");
+      const postedByCreate = document.createElement("h2");
+
+      postContents.push(contentValue);
+
+      postedByCreate.innerHTML = postedBy;
+      postContentCreate.innerHTML = contentValue;
+
+      if (document.querySelector(".post")) {
+        const firstChild = document.querySelector(".post");
+        allPosts.insertBefore(postDivCreate, firstChild);
+      } else allPosts.appendChild(postDivCreate);
+
+      postDivCreate.appendChild(postedByCreate);
+      postDivCreate.appendChild(postContentCreate);
+    }
+  });
 });
